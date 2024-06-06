eb2a2c1db8461c115b95551b2f6c5dc61cfa964b
Shepherd23333 <927561263l@gmail.com>
2024-06-06 12:21:06 +0800

    test

--- quat.cpp
+++ quat.cpp
@@ -1,20 +1,20 @@
 #include "quat.h"
 
 stack<string> s;
-int xuhao = 0;//用以记录生成第几个四元式
-int jilu = 0;//用以记录第几个中间变量的生成
-string siyuanshi[100];//存取四元式字符串数组
+int xuhao = 0;         // 用以记录生成第几个四元式
+int jilu = 0;          // 用以记录第几个中间变量的生成
+string siyuanshi[100]; // 存取四元式字符串数组
 string temp1;
 string temp2;
-string bo;//存取bool的比较符，要插动作
-int if1;//用于if四元式回填地址时使用的
+string bo; // 存取bool的比较符，要插动作
+int if1;   // 用于if四元式回填地址时使用的
 int if2;
-int while1;//用于while四元式回填地址时使用的
-int while2;//用于while四元式回填地址时使用的
+int while1; // 用于while四元式回填地址时使用的
+int while2; // 用于while四元式回填地址时使用的
 int while3;
 int while4;
-int for1;//用于while四元式回填地址时使用的
-int for2;//用于while四元式回填地址时使用的
+int for1; // 用于while四元式回填地址时使用的
+int for2; // 用于while四元式回填地址时使用的
 int for3;
 int for4;
 
@@ -22,9 +22,9 @@ int for4;
 int xuhao1 = 0;
 string siyuanshi1[100];
 
-string SYS[50][4];                  //数组四元式
-string SYS_new[50][4];              //优化过的数组四元式
-int SYS_new_len = 0;                  //优化过后的数组四元式的长度
+string SYS[50][4];     // 数组四元式
+string SYS_new[50][4]; // 优化过的数组四元式
+int SYS_new_len = 0;   // 优化过后的数组四元式的长度
 
 string youhua_quan[50][10];
 int youhua_quan_j_len[50];
@@ -32,22 +32,16 @@ int quan = 0;
 string youhua_chaozuo[50][4];
 int chaozuo = 0;
 
-
-string int2str(int& i)
+// 语法制导生成四元式
+void QUATJIA() // 加号生成四元式
 {
-    return to_string(i);
-}
-
-//语法制导生成四元式
-void QUATJIA()//加号生成四元式
-{
-    temp1 = s.top();//弹栈
+    temp1 = s.top(); // 弹栈
     s.pop();
     temp2 = s.top();
     s.pop();
-    string* p = new string;
-    *p = "t" + int2str(jilu);
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(+," + temp2 + "," + temp1 + "," + *p + ")";
+    string *p = new string;
+    *p = "t" + to_string(jilu);
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(+," + temp2 + "," + temp1 + "," + *p + ")";
     SYS[xuhao][0] = "+";
     SYS[xuhao][1] = temp2;
     SYS[xuhao][2] = temp1;
@@ -56,15 +50,15 @@ void QUATJIA()//加号生成四元式
     xuhao++;
 }
 
-void QUATJIAN()//减号生成四元式
+void QUATJIAN() // 减号生成四元式
 {
-    temp1 = s.top();//弹栈
+    temp1 = s.top(); // 弹栈
     s.pop();
     temp2 = s.top();
     s.pop();
-    string* p = new string;
-    *p = "t" + int2str(jilu);
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(-," + temp2 + "," + temp1 + "," + *p + ")";
+    string *p = new string;
+    *p = "t" + to_string(jilu);
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(-," + temp2 + "," + temp1 + "," + *p + ")";
     SYS[xuhao][0] = "-";
     SYS[xuhao][1] = temp2;
     SYS[xuhao][2] = temp1;
@@ -73,15 +67,15 @@ void QUATJIAN()//减号生成四元式
     xuhao++;
 }
 
-void QUATCHENGE()//乘号生成四元式
+void QUATCHENGE() // 乘号生成四元式
 {
-    temp1 = s.top();//弹栈
+    temp1 = s.top(); // 弹栈
     s.pop();
     temp2 = s.top();
     s.pop();
-    string* p = new string;
-    *p = "t" + int2str(jilu);
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(*," + temp2 + "," + temp1 + "," + *p + ")";
+    string *p = new string;
+    *p = "t" + to_string(jilu);
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(*," + temp2 + "," + temp1 + "," + *p + ")";
     siyuanshi1[xuhao1] = "(*," + temp2 + "," + temp1 + "," + *p + ")";
     SYS[xuhao][0] = "*";
     SYS[xuhao][1] = temp2;
@@ -92,16 +86,15 @@ void QUATCHENGE()//乘号生成四元式
     xuhao++;
 }
 
-
-void QUATCHU()///除号四元式 
+void QUATCHU() /// 除号四元式
 {
-    temp1 = s.top();//弹栈
+    temp1 = s.top(); // 弹栈
     s.pop();
     temp2 = s.top();
     s.pop();
-    string* p = new string;
-    *p = "t" + int2str(jilu);
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(/," + temp2 + "," + temp1 + "," + *p + ")";
+    string *p = new string;
+    *p = "t" + to_string(jilu);
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(/," + temp2 + "," + temp1 + "," + *p + ")";
     siyuanshi1[xuhao1] = "(/," + temp2 + "," + temp1 + "," + *p + ")";
     SYS[xuhao][0] = "/";
     SYS[xuhao][1] = temp2;
@@ -112,13 +105,13 @@ void QUATCHU()///除号四元式
     xuhao++;
 }
 
-void QUATFUZHI()//赋值语句生成四元式所需函数
+void QUATFUZHI() // 赋值语句生成四元式所需函数
 {
-    temp1 = s.top();//弹栈
+    temp1 = s.top(); // 弹栈
     s.pop();
     temp2 = s.top();
     s.pop();
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(=," + temp1 + "," + "_" + "," + temp2 + ")";
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(=," + temp1 + "," + "_" + "," + temp2 + ")";
     siyuanshi1[xuhao1] = "(=," + temp1 + "," + "_" + "," + temp2 + ")";
     SYS[xuhao][0] = "=";
     SYS[xuhao][1] = temp1;
@@ -128,30 +121,30 @@ void QUATFUZHI()//赋值语句生成四元式所需函数
     xuhao++;
 }
 
-//以下为条件语句和循环语句生成四元式部分所需函数
-void QUATBOOL() //bool类型表达式 
+// 以下为条件语句和循环语句生成四元式部分所需函数
+void QUATBOOL() // bool类型表达式
 {
-    temp1 = s.top();//弹栈
+    temp1 = s.top(); // 弹栈
     s.pop();
     temp2 = s.top();
     s.pop();
-    string* p = new string;
-    *p = "t" + int2str(jilu);
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(" + bo + "," + temp2 + "," + temp1 + "," + *p + ")";
+    string *p = new string;
+    *p = "t" + to_string(jilu);
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(" + bo + "," + temp2 + "," + temp1 + "," + *p + ")";
     SYS[xuhao][0] = bo;
     SYS[xuhao][1] = temp2;
     SYS[xuhao][2] = temp1;
     SYS[xuhao][3] = *p;
     xuhao++;
-    s.push(*p);//将bool表达式的结果存入栈中
+    s.push(*p); // 将bool表达式的结果存入栈中
 }
 
-void QUATIF()//if语句 
+void QUATIF() // if语句
 {
     temp1 = s.top();
     s.pop();
     string temp = "";
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(" + "if" + "," + temp1 + "," + "_" + ",";//此时s.pop就是bool表达式的结果
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(" + "if" + "," + temp1 + "," + "_" + ","; // 此时s.pop就是bool表达式的结果
     siyuanshi1[xuhao1] = temp + "(" + "if" + "," + temp1 + "," + "_" + ",";
     SYS[xuhao][0] = "if";
     SYS[xuhao][1] = temp1;
@@ -163,14 +156,14 @@ void QUATIF()//if语句
     xuhao1++;
 }
 
-void QUATELSE()//else语句 
+void QUATELSE() // else语句
 {
     string temp = "";
     int i = xuhao + 1;
     int j = xuhao1 + 1;
-    siyuanshi[if1] += int2str(i) + ")";
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(" + "el" + "," + "_" + "," + "_" + ",";
-    siyuanshi1[if2] += int2str(j) + ")";
+    siyuanshi[if1] += to_string(i) + ")";
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(" + "el" + "," + "_" + "," + "_" + ",";
+    siyuanshi1[if2] += to_string(j) + ")";
     siyuanshi1[xuhao1] = temp + "(" + "el" + "," + "_" + "," + "_" + ",";
     SYS[xuhao][0] = "el";
     SYS[xuhao][1] = "_";
@@ -182,12 +175,12 @@ void QUATELSE()//else语句
     xuhao1++;
 }
 
-void QUATIFEND()//if语句结束 
+void QUATIFEND() // if语句结束
 {
     string temp = "";
-    siyuanshi[if1] += int2str(xuhao) + ")";
-    siyuanshi1[if2] += int2str(xuhao1) + ")";
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(" + "ie" + "," + "_" + "," + "_" + "," + "_" + ")";
+    siyuanshi[if1] += to_string(xuhao) + ")";
+    siyuanshi1[if2] += to_string(xuhao1) + ")";
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(" + "ie" + "," + "_" + "," + "_" + "," + "_" + ")";
     siyuanshi1[xuhao1] = temp + "(" + "ie" + "," + "_" + "," + "_" + "," + "_" + ")";
     SYS[xuhao][0] = "ie";
     SYS[xuhao][1] = "_";
@@ -197,12 +190,12 @@ void QUATIFEND()//if语句结束
     xuhao++;
 }
 
-void QUATWHILE()//while语句 
+void QUATWHILE() // while语句
 {
     string temp = "";
     while1 = xuhao;
     while3 = xuhao1;
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(" + "wh" + "," + "_" + "," + "_" + "," + "_" + ")";
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(" + "wh" + "," + "_" + "," + "_" + "," + "_" + ")";
     siyuanshi1[xuhao1] = temp + "(" + "wh" + "," + "_" + "," + "_" + "," + "_" + ")";
     SYS[xuhao][0] = "wh";
     SYS[xuhao][1] = "_";
@@ -212,14 +205,14 @@ void QUATWHILE()//while语句
     xuhao++;
 }
 
-void QUATDO()//do语句 
+void QUATDO() // do语句
 {
     while2 = xuhao;
     while4 = xuhao1;
     string temp = "";
     temp1 = s.top();
     s.pop();
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(" + "do" + "," + temp1 + "," + "_" + ",";//bool那个区已经将bool表达式的结果存进去了，即s.pop
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(" + "do" + "," + temp1 + "," + "_" + ","; // bool那个区已经将bool表达式的结果存进去了，即s.pop
     siyuanshi1[xuhao1] = temp + "(" + "do" + "," + temp1 + "," + "_" + ",";
     SYS[xuhao][0] = "do";
     SYS[xuhao][1] = temp1;
@@ -229,17 +222,17 @@ void QUATDO()//do语句
     xuhao++;
 }
 
-void QUATWHILEEND()//while语句结束 
+void QUATWHILEEND() // while语句结束
 {
     string temp = "";
     int i = xuhao + 1;
     int j = while1 + 1;
     int k = xuhao1 + 1;
     int m = while3 + 1;
-    siyuanshi[while2] += int2str(i) + ")";
-    siyuanshi1[while4] += int2str(k) + ")";
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(" + "we" + "," + "_" + "," + "_" + "," + int2str(j) + ")";
-    siyuanshi1[xuhao1] = temp + "(" + "we" + "," + "_" + "," + "_" + "," + int2str(m) + ")";
+    siyuanshi[while2] += to_string(i) + ")";
+    siyuanshi1[while4] += to_string(k) + ")";
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(" + "we" + "," + "_" + "," + "_" + "," + to_string(j) + ")";
+    siyuanshi1[xuhao1] = temp + "(" + "we" + "," + "_" + "," + "_" + "," + to_string(m) + ")";
     SYS[xuhao][0] = "we";
     SYS[xuhao][1] = "_";
     SYS[xuhao][2] = "_";
@@ -248,12 +241,12 @@ void QUATWHILEEND()//while语句结束
     xuhao++;
 }
 
-void QUATFOR()//for语句 
+void QUATFOR() // for语句
 {
     string temp = "";
     for1 = xuhao;
     for3 = xuhao1;
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(" + "for" + "," + "_" + "," + "_" + "," + "_" + ")";
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(" + "for" + "," + "_" + "," + "_" + "," + "_" + ")";
     siyuanshi1[xuhao1] = temp + "(" + "for" + "," + "_" + "," + "_" + "," + "_" + ")";
     SYS[xuhao][0] = "for";
     SYS[xuhao][1] = "_";
@@ -263,14 +256,14 @@ void QUATFOR()//for语句
     xuhao++;
 }
 
-void QUATDOFOR()//do for语句 
+void QUATDOFOR() // do for语句
 {
     for2 = xuhao;
     for4 = xuhao1;
     string temp = "";
     temp1 = s.top();
     s.pop();
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(" + "do" + "," + temp1 + "," + "_" + ",";//bool那个区已经将bool表达式的结果存进去了，即s.pop
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(" + "do" + "," + temp1 + "," + "_" + ","; // bool那个区已经将bool表达式的结果存进去了，即s.pop
     siyuanshi1[xuhao1] = temp + "(" + "do" + "," + temp1 + "," + "_" + ",";
     SYS[xuhao][0] = "do";
     SYS[xuhao][1] = temp1;
@@ -280,17 +273,17 @@ void QUATDOFOR()//do for语句
     xuhao++;
 }
 
-void QUATFOREND()//for语句结束 
+void QUATFOREND() // for语句结束
 {
     string temp = "";
     int i = xuhao + 1;
     int j = for1 + 1;
     int k = xuhao1 + 1;
     int m = for3 + 1;
-    siyuanshi[for2] += int2str(i) + ")";
-    siyuanshi1[for4] += int2str(k) + ")";
-    siyuanshi[xuhao] = "QT[" + int2str(xuhao) + "]:(" + "fe" + "," + "_" + "," + "_" + "," + int2str(j) + ")";
-    siyuanshi1[xuhao1] = temp + "(" + "fe" + "," + "_" + "," + "_" + "," + int2str(m) + ")";
+    siyuanshi[for2] += to_string(i) + ")";
+    siyuanshi1[for4] += to_string(k) + ")";
+    siyuanshi[xuhao] = "QT[" + to_string(xuhao) + "]:(" + "fe" + "," + "_" + "," + "_" + "," + to_string(j) + ")";
+    siyuanshi1[xuhao1] = temp + "(" + "fe" + "," + "_" + "," + "_" + "," + to_string(m) + ")";
     SYS[xuhao][0] = "fe";
     SYS[xuhao][1] = "_";
     SYS[xuhao][2] = "_";
